7e92148236c9616cc26509c328599d982a1d8c5d
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-04-28 18:27:19 +0100

    feat: handling asynchronous code

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -25,13 +25,23 @@ export const subtract = (wartosc) => {
     return {
         type: SUBTRACT,
         value: wartosc
-    }
-};
-export const storeResult = (res) => {
+    };
+}
+
+export const saveResult = (res) => {
     return {
         type: STORE_RESULT,
         result: res
     }
+};
+
+export const storeResult = (res) => {
+    return dispatch  => {
+        setTimeout( () => {
+            dispatch(saveResult(res));
+        }, 2000);
+    }
+
 };
 export const deleteResult = (resElId) => {
     return {
