87690defcb0592b1fc9a4be7f5583e0cb1412580
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-04-29 17:41:36 +0100

    feat: using utility functions

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,6 +1,6 @@
 import { useDebugValue } from "react";
 import * as actionTypes from '../actions/actionTypes';
-
+import {updateObject} from '../utility';
 
 const initialState = {
     counter: 0
@@ -10,31 +10,14 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
 
         case actionTypes.INCREMENT: 
-                const newState = Object.assign({}, state);
-                newState.counter = state.counter + 1;
-                return newState;
-
-        case actionTypes.DECREMENT: {
-            return {
-                ...state,
-                counter: state.counter - 1
-            }
-        }
-        case actionTypes.ADD: {
-            return {
-                ...state,
-                counter: state.counter + action.value
-            }
-        }
-        case actionTypes.SUBTRACT: {
-            return {
-                ...state,
-                counter: state.counter - action.value
-            }
-        }
-       
+            return updateObject(state, {counter: state.counter + 1});
+        case actionTypes.DECREMENT: 
+            return updateObject(state, {counter: state.counter - 1});
+        case actionTypes.ADD: 
+            return updateObject(state, {counter: state.counter + action.value});
+        case actionTypes.SUBTRACT: 
+            return updateObject(state, {counter: state.counter - action.value});
     }
-
     return state;
 };
 
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,6 +1,6 @@
 import { useDebugValue } from "react";
 import * as actionTypes from '../actions/actionTypes';
-
+import { updateObject } from '../utility';
 
 const initialState = {
     results: []
@@ -10,17 +10,11 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT: {
             // here we can change data
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: action.result})
-            }
+            return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})});
         }
         case actionTypes.DELETE_RESULT: {
             const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return {
-                ...state,
-                results: updatedArray
-            }
+            return updateObject (state, {results: updatedArray});
         }
     }
 
